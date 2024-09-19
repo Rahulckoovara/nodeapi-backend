ad0eb672669cf9a09d34d7b8d64ae2132c96b1ea
Rahulckoovara <rahulckoovara1@gmail.com>
2024-09-19 11:45:00 +0530

    removed the toke n validation

--- routes/post.js
+++ routes/post.js
@@ -1,53 +1,45 @@
 const express = require("express");
 
-const postControllers= require("../controllers/post");
+const postControllers = require("../controllers/post");
 const { createPostValidator } = require("../validators");
 const { requireLogin, authenticateToken } = require("../middlewares/auth");
 const router = express.Router();
 
-router.get("/",requireLogin, postControllers.getposts);
-router.post("/post", requireLogin, createPostValidator, postControllers.createPost);
-
+router.get("/", requireLogin, postControllers.getposts);
+router.post(
+  "/post",
+  requireLogin,
+  createPostValidator,
+  postControllers.createPost
+);
 
 //1 Route for user registration
 router.post("/register", postControllers.register); // Route for user registration
 
 //2 Route for user login
-router.post("/login", postControllers.login); 
+router.post("/login", postControllers.login);
 
 //3get the registered data
-router.get("/userdetails",authenticateToken, postControllers.getUsers);
+router.get("/userdetails", postControllers.getUsers);
 
 //4fetch the user data with the user id
-router.get("/userdetails/:id",authenticateToken, postControllers.getUserById);
-
+router.get("/userdetails/:id", postControllers.getUserById);
 
 //5update the user data with the user id
 router.put("/userdetails/:id", postControllers.updateUser);
 
-
 //6registering the asset with details
-router.post("/assetdetails",postControllers.assets);
-
+router.post("/assetdetails", postControllers.assets);
 
 //7getting the user details by the if from the user
-router.get("/assets/:userId",postControllers.getUserAssetById);
-
+router.get("/assets/:userId", postControllers.getUserAssetById);
 
 //8get all the asset details
-router.get("/assets",postControllers.getAllAssets);
-
-
-
-
+router.get("/assets", postControllers.getAllAssets);
 
 //logout
 //router.post("/logout",authenticateToken, postControllers.logout);
 
 // router.post("/logout",postControllers.logout);
 
-
-
-
 module.exports = router;
-
